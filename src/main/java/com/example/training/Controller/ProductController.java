431a314e25d3c76b01dce5976acf3ab1c62d5a68
Salwa KHALED <salwakha940@gmail.com>
2025-02-20 14:25:04 +0100

    Add product class, repository, service and controller

--- src/main/java/com/example/training/Controller/ProductController.java
+++ src/main/java/com/example/training/Controller/ProductController.java
@@ -2,20 +2,25 @@ package com.example.training.Controller;
 
 
 import com.example.training.Model.Product;
-import com.example.training.Model.User;
 import com.example.training.Repository.ProductRepository;
 import com.example.training.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-
+    public ProductRepository productRepository;
     public ProductService productService;
+
+
 
     @Autowired
     public ProductController(ProductService productService) {
@@ -42,5 +47,21 @@ public class ProductController {
         return productService.getAllProducts();
     }
 
+    @GetMapping("/{id}")
+    public Optional<Product> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+    @GetMapping("/getproduct/{id}")
+    public Product getProduct(@PathVariable Long id) {
+        return productService.findProductById(id);
+    }
+
+    @GetMapping("/paginated")
+    public Page<Product> getPaginatedProducts(@RequestParam int page, @RequestParam int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return productService.getProducts(page, size);
+    }
+
 
 }
+
--- src/main/java/com/example/training/Service/ProductService.java
+++ src/main/java/com/example/training/Service/ProductService.java
@@ -3,6 +3,9 @@ package com.example.training.Service;
 
 import com.example.training.Model.Product;
 import com.example.training.Repository.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,15 +32,27 @@ public class ProductService {
     public Optional<Product> getProductById(Long id) {
         return productRepository.findById(id);
     }
+    public Product findProductById(Long id) {
+        return productRepository.findById(id).get();
+    }
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
-    public void deleteProduct(Long id) {
-        productRepository.deleteById(id);
+    public Page<Product> getProducts(int page, int size ) {
+        Pageable pageable = PageRequest.of(page,size);
+        return productRepository.findAll(pageable);
     }
 
+    public void deleteProduct(Long id) {
+        if(productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+            System.out.println("Successfully delete");
+        }else{
+            throw new RuntimeException("Product Not Found");
+        }
+    }
 
 
 
